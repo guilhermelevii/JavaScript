4708c7f81ba80c279f203cc6d120d8c9dcacbf3d
Guilherme Levi <134446038+guilhermelevii@users.noreply.github.com>
2024-01-17 11:04:07 -0300

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,4 +1,4 @@
-console.log('O console funcionou corretamente')
 var vel = 210.00
 console.log(`A velocidade que o carro estava era de ${vel} Km/h.`)
+console.log(``)
 console.log(`Dirija sempre com cinto de segurança.`)
