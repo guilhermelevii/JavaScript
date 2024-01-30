df8a7cf54e9c286cd11a48d26f24294ed7546f0c
Guilherme Levi <134446038+guilhermelevii@users.noreply.github.com>
2024-01-30 15:49:01 -0300

    criando algunas exemplos em js

---
+++ aula12/ex013.js
@@ -0,0 +1,20 @@
+var idade = 17
+console.log(`Você tem ${idade} anos de idade.`)
+if(idade >= 18){
+    console.log('Você é maior de idade')
+} else{
+    console.log('Você é menor de idade.')
+}
+
+var num = -5
+console.log(`O número escolhido foi ${num}`)
+if(num > 0){
+    console.log('O número é positivo')
+} else if(num < 0){
+    console.log('O número é negativo')
+}
+
+var valor = 650;
+console.log(`O valor de quilômetros escolhido foi de ${valor} Km`);
+var formula = (valor * 1000);
+console.log(`O valor de ${valor} quilômetro é o equivalente a ${formula} metros. `)
