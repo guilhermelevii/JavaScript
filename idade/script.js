d38e04d5ccfcb94cc8c33975454c80739a6559ab
Guilherme Levi <134446038+guilhermelevii@users.noreply.github.com>
2024-03-05 17:23:40 -0300

    work

---
+++ idade/script.js
@@ -0,0 +1,6 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.querySelector('res') // confirmar se está funcionando no codigo dos estudos é um ID e aqui no caso coloquei como CLASS.
+}
